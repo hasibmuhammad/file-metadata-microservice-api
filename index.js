e2bfff1de08107cd666a74e1e47674cdd3266bbd
Hasib Muhammad <hasibmu99@gmail.com>
2024-06-05 03:55:46 +0000

    File metadata microservice - done

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
 
-var app = express();
+const upload = multer({dest: "uploads/"});
+
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +14,10 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  const {originalname, mimetype, size} = req.file;
+  res.json({name: originalname, type: mimetype, size})
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
